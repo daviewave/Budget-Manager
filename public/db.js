44cf873623e04c1caaf3acd71e086059e81cabde
David Silveira <daviewave@gmail.com>
2021-12-22 03:29:04 -0500

    added db.js to public folder along with code to handle indexedDB and complete 'transactions' with the db

---
+++ public/db.js
@@ -0,0 +1,51 @@
+const indexdb =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
+
+let db;
+
+const request = indexdb.open("budget", 1);
+
+request.onupgradeneeded = ({ target }) => {
+  let db = target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onerror = function (e) {
+  console.log("There was an error");
+};
+
+request.onsuccess = ({ target }) => {
+  db = target.result;
+  if (navigator.onLine) {
+    checkDb();
+  }
+};
+
+const transaction = db.transaction(["pending"], "readwrite");
+const store = transaction.objectStore("pending");
+
+const saveTransaction = () => {
+  store.add();
+};
+
+const checkDb =
+  ("/",
+  async (req, res) => {
+    const getAll = store.getAll();
+
+    getAll.onsuccess = async () => {
+      try {
+        const response = await fetch("/api/transaction/bulk");
+        res.status(200).json(response);
+        store.clear();
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    };
+  });
+
+window.addEventListener("online", checkDb);
